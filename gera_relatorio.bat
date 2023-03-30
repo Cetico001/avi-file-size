611735e4e4ee019c3a276de8b73770e5351a2825
Cetico001 <103577669+Cetico001@users.noreply.github.com>
2023-03-30 10:04:23 -0300

    Update invalid years
    As câmeras usadas precisam ser sempre sincronizadas ao celular para gravar com o horário certo, mas algumas vezes ela dessincronizam com o celular gerando uma legenda com datas antes do ano 2000, então atualizei o código para ele poder identificar essa mudança, adicionando eles ao relatório de erros também.

--- gera_relatorio.bat
+++ gera_relatorio.bat
@@ -1,6 +1,7 @@
 @echo off
 echo Gerando arquivos...
 setlocal EnableDelayedExpansion
+chcp 65001
 
 rem definindo as variaveis de caminho e arquivos a serem criados
 set "dir=%CD%"
@@ -16,11 +17,14 @@ set /a novosdias1=0
 set /a auth=0
 set /a fcont=0
 set /a xcont=0
+set /a sinc=0
+set /a comparar=0
 
 rem verifica se existe
 if exist "%relatorio%" del "%relatorio%"
 if exist "%verificar_dados%" del "%verificar_dados%"
-  
+if exist "%~dp0\a.txt" del "%~dp0\a.txt"
+
 rem cria cabecalho do arquivo relatorio
 echo Verificar,Data, Hora, Tamanho, Diretorio, Nome >> "%verificar_dados%"
   
@@ -63,69 +67,115 @@ for /f "delims=" %%f in ('dir /s /b /od "%dir%\*.avi"') do (
   set /a "horant=!hora!"
   
   for /f "tokens=2 delims=_" %%a in ("!fullname!") do set "data_hora=%%a"
-    set "ano=!data_hora:~0,4!"
-    set "mes=!data_hora:~4,2!"
-    set "dia=!data_hora:~6,2!"
-    set "hora=!data_hora:~8,2!"
-    set "minuto=!data_hora:~10,2!"
-    set "segundo=!data_hora:~12,2!"
-  
-  set "dd=!dia!"
-  set "hh=!hora!"
-  set "mm=!minuto!"
-  
-  rem conversão
-  if !minuto! lss 10 (
-    set /a "minuto=1!minuto!-100"
-  ) 
-  if !hora! lss 10 (
-    set /a "hora=1!hora!-100"
-  ) 
-  if !dia! lss 10 (
-    set /a "dia=1!dia!-100"
-  ) 
-  if !filesize! lss 1073741824 (
-    set "filesize=!filesize:~0,-6! MB"
+  set /a "verif=!data_hora:~0,4!+0"
+  if !verif! geq 2000 (
+     set "ano=!data_hora:~0,4!"
+     set "mes=!data_hora:~4,2!"
+     set "dia=!data_hora:~6,2!"
+     set "hora=!data_hora:~8,2!"
+     set "minuto=!data_hora:~10,2!"
+     set "segundo=!data_hora:~12,2!"
+      
+     set "dd=!dia!"
+     set "hh=!hora!"
+     set "mm=!minuto!"
+      
+     rem conversão
+     if !minuto! lss 10 (
+       set /a "minuto=1!minuto!-100"
+     ) 
+     if !hora! lss 10 (
+       set /a "hora=1!hora!-100"
+     ) 
+     if !dia! lss 10 (
+        set /a "dia=1!dia!-100"
+     ) 
+     if !filesize! lss 1073741824 (
+        set "filesize=!filesize:~0,-6! MB"
+     ) else (
+        set /a "filesize=!filesize! / 1073741824"
+        set "filesize=!filesize! GB"
+     )
+      
+     if !cont! geq 1 if !hora! leq !horapos! if !hora! geq !horant! (
+       if !minuto! geq !novosminutos! (
+        if !hora! equ !novashoras! (
+         set "Verificar=Verificar"
+         echo Faltando Video >> "%verificar_dados%"
+         set /a auth=1
+         set /a "fcont=fcont+1"
+         echo Caminho a verificar: %%~dpf!fullname! >> "%relatorio%"    
+        )
+       )
+       if !hora! gtr !novashoras! if !auth! equ 0 (
+         set "Verificar=Verificar"
+         echo Faltando Video >> "%verificar_dados%"
+         set /a auth=1
+         set /a "fcont=fcont+1"
+         echo Caminho a verificar: %%~dpf!fullname! >> "%relatorio%"
+       )
+       if !minuto! lss !ante! if !auth! equ 0 (
+        if !hora! leq !novashoras1! (
+         set "Verificar=Verificar"
+         echo Faltando Video >> "%verificar_dados%"
+         set /a auth=1
+         set /a "fcont=fcont+1"
+         echo Caminho a verificar: %%~dpf!fullname! >> "%relatorio%"
+        )
+       ) 
+     )
+      	
+     set /a auth=0
+     set /a "cont=!cont!+1"
+
+     echo !Verificar!, !ano!-!mm!-!dd!, !hh!:!mm!:!segundo!, !filesize!, %%~dpf, !fullname! >> "%verificar_dados%"
   ) else (
-    set /a "filesize=!filesize! / 1073741824"
-    set "filesize=!filesize! GB"
+	
+      for /f "tokens=1 delims=_" %%a in ("!fullname!") do set "rec=%%a"
+	set /a string=0
+	for /L %%i in (0,1,100) do (
+    	  set "letra=!rec:~%%i,1!"
+    	  if not "!letra!"=="" set /a string+=1
+	)
+
+	if !sinc! gtr 0 (
+	 set /a comparar=!maiornumero!
+	)
+	if !string! equ 4 (
+	  set /a num=!rec:~3,1!
+	  if !num! gtr 3 (
+	   set vet[!num!]=%%~dpf!fullname!
+	   set /a "sinc=!sinc!+1"
+	  )
+	) else (
+	     if !string! equ 5 (
+	      set /a num=!rec:~3,2!
+	      set vet[!num!]=%%~dpf!fullname!
+		set /a "sinc=!sinc!+1"
+	     ) else (
+		if !string! equ 6 (
+	 	 set /a num=!rec:~3,3!
+	  	 set vet[!num!]=%%~dpf!fullname!
+		 set /a "sinc=!sinc!+1"
+ 	  	) 
+	     )
+	  )
+
+	if !num! gtr !comparar! (
+	 set /a maiornumero=!vet[!num!]!
+	)
+      )
+)
+
+
+for /L %%i in (3,1,1000) do (
+
+  if not "!vet[%%i]!"=="" (
+    echo Video desincronizado: !vet[%%i]! >> "%relatorio%"
   )
-  
-  if !cont! geq 1 if !hora! leq !horapos! if !hora! geq !horant! (
-   if !minuto! geq !novosminutos! (
-    if !hora! equ !novashoras! (
-     set "Verificar=Verificar"
-     echo Faltando Video >> "%verificar_dados%"
-     set /a auth=1
-     set /a "fcont=fcont+1"
-     echo Caminho a verificar: %%~dpf!fullname! >> "%relatorio%"    
-    )
-   )
-   if !hora! gtr !novashoras! if !auth! equ 0 (
-     set "Verificar=Verificar"
-     echo Faltando Video >> "%verificar_dados%"
-     set /a auth=1
-     set /a "fcont=fcont+1"
-     echo Caminho a verificar: %%~dpf!fullname! >> "%relatorio%"
-   )
-   if !minuto! lss !ante! if !auth! equ 0 (
-    if !hora! leq !novashoras1! (
-     set "Verificar=Verificar"
-     echo Faltando Video >> "%verificar_dados%"
-     set /a auth=1
-     set /a "fcont=fcont+1"
-     echo Caminho a verificar: %%~dpf!fullname! >> "%relatorio%"
-    )
-   ) 
-  )
-  	
-  set /a auth=0
-  echo !Verificar!, !ano!-!mm!-!dd!, !hh!:!mm!:!segundo!, !filesize!, %%~dpf, !fullname! >> "%verificar_dados%"
-  
-  set /a "cont=!cont!+1"
-  
-) 
-  
+)
+
 echo Quantidade de videos faltando: !fcont! >> "%relatorio%"
 echo Quantidade de videos corrompidos: !xcont! >> "%relatorio%"
+echo Quantidade de videos desincronizados: !sinc! >> "%relatorio%"
 
