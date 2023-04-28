acb0a83af65dbf4ae6052eb97185ad59fc30d515
Cetico001 <103577669+Cetico001@users.noreply.github.com>
2023-04-28 11:59:09 -0300

    Big Update
    Na minha empresa, tem 2 tipos de gravação, uma é quando a câmera divide os vídeos em pastas diferentes, outra é quando você vai usar na própria pasta, tipo 1 e 2, geralmente a do tipo 1 que é a que só tem uma pasta tem sequenciais de Rec(Rec1,Rec2,Rec3), já a tipo 2 que faz uma divisão de pastas só tem o Rec2 todos os vídeos. Então é importante essa separação, tendo em vista que também os vídeos do tipo 2, de acordo com o FTR da pasta atual, o fuso horário pode mudar, então foi adicionado uma logica com base no FTR da pasta para arrumar o fuso horário e pegar o número do FTR atual lendo a pasta.
    
    Foi adicionado também uma listagem dos vídeos dessincronizados, mas funciona apenas com o tipo 1, já que tem sequências de rec, a listagem não estava listando em ordem, estava toda desordenada, então criei um "vetor" em bat, para conseguir armazenar tudo neles, e logo depois abro um for para listá-los na ordem certa.
    
    Há também uma logica sobre os vídeos para calcular quantos minutos tem no total, se foi perdido algum tempo ou algo semelhante, mas isso é algo bem especifico.

--- gera_relatorio.bat
+++ gera_relatorio.bat
@@ -5,25 +5,71 @@ chcp 65001
 
 rem definindo as variaveis de caminho e arquivos a serem criados
 set "dir=%CD%"
+set "Erros=%~dp0\RelatorioErros.txt"
 set "relatorio=%~dp0\relatorio.txt"
 set "verificar_dados=%~dp0\verificar_dados.txt"
 set /a cont=0
-set /a novosminutos=0
-set /a ante=0
+set /a conferir=0
+set /a horaconf=0
+set /a horaiconf=0
+set /a horafconf=0
 set /a novashoras1=0
 set /a novashoras=0
 set /a novosdias=0
 set /a novosdias1=0
-set /a auth=0
 set /a fcont=0
 set /a xcont=0
+set /a minfaltando=0
 set /a sinc=0
 set /a comparar=0
+set /a vrf=0
+set /p tipovideo=Tipo de video(1-Sequencial sem pastas. 2-Dividido por pastas.):
+set /p tinicio=Digite o horario inicio da filmagem: 
+set /p tfinal=Digite o horario final da filmagem: 
+set /p periodohr=Digite a quantidade de horas da filmagem: 
+set /a periodomin=!periodohr!*60
+set /a qntdvideos=(!periodomin!*10)/99
+set /a fuso=0
+set /a diateste=0
+
+for /f "tokens=*" %%a in ("%dir%") do set ultimos_numeros=%%~na
+set ultimos_numeros=%ultimos_numeros:*-FTR=%
+set /a ftrcode=!ultimos_numeros!
+
+if !ftrcode! lss 10 (
+ set /a "ftrcode=1!ftrcode!-100"
+) 
+
+if !tipovideo! equ 2 (
+ for /L %%i in (302,1,310) do (
+  if !ftrcode! equ %%i (
+   set /a fuso=8
+  ) 
+ )
+ for /L %%i in (1,1,11) do (
+  if !ftrcode! equ %%i (
+   set /a fuso=8
+   if !ftrcode! equ 3 (
+    set /a fuso=3
+   )
+  ) 
+ )
+ if !ftrcode! equ 15 (
+  set /a fuso=3
+ )
+ if !ftrcode! equ 16 (
+  set /a fuso=3
+ )
+ if !ftrcode! equ 301 (
+  set /a fuso=3
+ )
+)
 
 rem verifica se existe
 if exist "%relatorio%" del "%relatorio%"
 if exist "%verificar_dados%" del "%verificar_dados%"
 if exist "%~dp0\a.txt" del "%~dp0\a.txt"
+if exist "%Erros%" del "%Erros%"
 
 rem cria cabecalho do arquivo relatorio
 echo Verificar,Data, Hora, Tamanho, Diretorio, Nome >> "%verificar_dados%"
@@ -36,29 +82,8 @@ for /f "delims=" %%f in ('dir /s /b /od "%dir%\*.avi"') do (
   set "Verificar=Ok"
   if !filesize! equ 0 (
     set "Verificar=Erro"
-    echo Corrompido: %%~dpf!fullname! >> "%relatorio%"
+    echo Corrompido: %%~dpf!fullname! >> "%Erros%"
     set /a "xcont=xcont+1"
-  )
-  
-  if !cont! geq 1 (
-   set /a "novosminutos=!minuto!+11"
-   set /a "novashoras=!hora!"
-   if !novosminutos! geq 60 (
-     set /a "novosminutos=!novosminutos!-60"
-     set /a "novashoras=!hora!+1"
-     if !novashoras! geq 24 (
-       set /a "novosdias=!dia!+1"
-     )
-   )
-   set /a "ante=!minuto!+8"
-   set /a "novashoras1=!hora!"
-   if !ante! geq 60 (
-     set /a "ante=!ante!-60"
-     set /a "novashoras1=!hora!+1"
-     if !novashoras1! geq 24 (
-       set /a "novosdias1=!dia!+1"
-     )
-   )
   )
   
   rem echo !novashoras1!:!ante!, !novashoras!:!novosminutos! >> "%relatorio%"
@@ -76,11 +101,16 @@ for /f "delims=" %%f in ('dir /s /b /od "%dir%\*.avi"') do (
      set "minuto=!data_hora:~10,2!"
      set "segundo=!data_hora:~12,2!"
       
-     set "dd=!dia!"
-     set "hh=!hora!"
      set "mm=!minuto!"
-      
+     set "ss=!segundo!"
+     set "hh=!hora!"
+     set "dd=!dia!"
+
+
      rem conversão
+     if !segundo! lss 10 (
+       set /a "segundo=1!segundo!-100"
+     ) 
      if !minuto! lss 10 (
        set /a "minuto=1!minuto!-100"
      ) 
@@ -96,48 +126,124 @@ for /f "delims=" %%f in ('dir /s /b /od "%dir%\*.avi"') do (
         set /a "filesize=!filesize! / 1073741824"
         set "filesize=!filesize! GB"
      )
-      
-     if !cont! geq 1 if !hora! leq !horapos! if !hora! geq !horant! (
-       if !minuto! geq !novosminutos! (
-        if !hora! equ !novashoras! (
-         set "Verificar=Verificar"
-         echo Faltando Video >> "%verificar_dados%"
-         set /a auth=1
-         set /a "fcont=fcont+1"
-         echo Caminho a verificar: %%~dpf!fullname! >> "%relatorio%"    
-        )
-       )
-       if !hora! gtr !novashoras! if !auth! equ 0 (
-         set "Verificar=Verificar"
-         echo Faltando Video >> "%verificar_dados%"
-         set /a auth=1
-         set /a "fcont=fcont+1"
-         echo Caminho a verificar: %%~dpf!fullname! >> "%relatorio%"
-       )
-       if !minuto! lss !ante! if !auth! equ 0 (
-        if !hora! leq !novashoras1! (
-         set "Verificar=Verificar"
-         echo Faltando Video >> "%verificar_dados%"
-         set /a auth=1
-         set /a "fcont=fcont+1"
-         echo Caminho a verificar: %%~dpf!fullname! >> "%relatorio%"
-        )
-       ) 
-     )
-      	
-     set /a auth=0
-     set /a "cont=!cont!+1"
 
-     echo !Verificar!, !ano!-!mm!-!dd!, !hh!:!mm!:!segundo!, !filesize!, %%~dpf, !fullname! >> "%verificar_dados%"
+     if !diateste! equ 0 (
+	set /a diateste=1
+      if !tinicio! lss !hora! (
+	 set /a diainicio=!dia!+1
+      ) else (
+	 set /a diainicio=!dia!
+      )
+     )
+
+     rem arrumando fuso horario
+     if !tipovideo! equ 2 (
+	if !fuso! equ 3 (
+	 set /a hora=!hora!-fuso
+	 if !hora! lss 0 (
+	  set /a hora=!hora!+24
+	  set /a dia=!dia!-1
+	 )
+	)
+	if !fuso! equ 8 (
+	 set /a hora=!hora!+fuso
+	 if !hora! gtr 23 (
+	  set /a hora=!hora!-24
+	  set /a dia=!dia!+1
+	 )
+	)
+      if !hora! lss 10 (
+       set "hh=0!hora!"
+      ) else (
+	 set "hh=!hora!"
+      )
+
+      if !dia! lss 10 (
+       set "dd=0!dia!"
+      ) else (
+	 set "dd=!dia!"
+      ) 
+     )
+
+     if !conferir! equ 0 (
+      set /a "horaconf=!hora!+1"
+	if !horaconf! equ 24 (
+	 set /a horaconf=0
+	)
+      if !tinicio! equ !horaconf! (
+	 if !minuto! geq 50 (
+	  set /a horaiconf=!minuto!-50
+	  set /a conferir=1
+	 )
+      )
+	if !hora! geq !tinicio! (
+	 if !dia! equ !diainicio! (
+	  set /a conferir=1
+	  set /a minfaltando=!minuto!
+	  set /a segfaltando=!segundo!
+	  echo !minfaltando!:!segfaltando! minutos faltando antes desse caminho: %%~dpf!fullname! >> "%Erros%"
+	 )
+	)  
+     )
+
+     if !conferir! equ 1 (
+      set /a horaconf=!hora!+1
+      if !tfinal! equ !horaconf! (
+	 if !minuto! geq 50 (
+	  set /a horafconf=!minuto!-50
+	  set /a conferir=2
+	 )
+      )  
+     )
+
+     if !cont! geq 1 if !hora! leq !horapos! if !hora! geq !horant! (
+
+	   if !hora! equ !horas! (
+	    set /a minutos=!minuto!-!minutos!
+	   )
+	   if !hora! gtr !horas! (
+	    set /a horas=!hora!-!horas!
+	    set /a conversao=!horas!*60
+	    set /a minutos=!minuto!+!conversao!-!minutos!
+	   )
+	   if !hora! lss !horas! (
+	    set /a dias=!dia!-!dias!
+	    set /a dias=!dias!*24
+	    set /a horas=!hora!+!dias!-!horas!
+	    set /a conversao=!horas!*60
+	    set /a minutos=!minuto!+!conversao!-!minutos!	
+	   )
+	   if !minutos! gtr 15 (
+	    set /a minutos=2+!minutos!
+	    set /a minutos=!minutos!/10
+	    set /a minutos=!minutos!-1
+          set "Verificar=Verificar"
+          echo Faltando Video >> "%verificar_dados%"
+          set /a "fcont=!fcont!+!minutos!"
+          echo !minutos! video/s faltando antes desse caminho: %%~dpf!fullname! >> "%Erros%"
+	   )
+     )
+     set /a segundos=!segundo!
+     set /a minutos=!minuto!
+     set /a horas=!hora!
+     set /a dias=!dia!
+     if !vrf! equ 0 (
+	set "horainic=Data e hora do inicio dos videos !ano!-!mes!-!dd!, !hh!:!mm!:!ss!"
+	set /a vrf=1
+     )
+
+     if !conferir! equ 1 (
+      set /a "cont=!cont!+1"
+     )
+
+     echo !Verificar!, !ano!-!mes!-!dd!, !hh!:!mm!:!ss!, !filesize!, %%~dpf, !fullname! >> "%verificar_dados%"
   ) else (
-	
       for /f "tokens=1 delims=_" %%a in ("!fullname!") do set "rec=%%a"
 	set /a string=0
 	for /L %%i in (0,1,100) do (
     	  set "letra=!rec:~%%i,1!"
     	  if not "!letra!"=="" set /a string+=1
 	)
-
 	if !sinc! gtr 0 (
 	 set /a comparar=!maiornumero!
 	)
@@ -160,22 +266,66 @@ for /f "delims=" %%f in ('dir /s /b /od "%dir%\*.avi"') do (
  	  	) 
 	     )
 	  )
+	
+	 if !num! gtr !comparar! (
+	  set /a maiornumero=!vet[!num!]!
+	 )
 
-	if !num! gtr !comparar! (
-	 set /a maiornumero=!vet[!num!]!
-	)
+	 if !tipovideo! equ 2 (
+	  echo Video desincronizado: %%~dpf!fullname! >> "%Erros%"
+	 )
       )
 )
-
 
 for /L %%i in (3,1,1000) do (
 
   if not "!vet[%%i]!"=="" (
-    echo Video desincronizado: !vet[%%i]! >> "%relatorio%"
+    echo Video desincronizado: !vet[%%i]! >> "%Erros%"
   )
+
 )
 
+set /a minquantidade=0
+set /a resto=0
+set /a minquantidade=!minfaltando!/10
+set /a resto=!minfaltando!-(!minquantidade!*10)
+set /a cont=!cont!+!minquantidade!
+set /a fcont=!minquantidade!+!fcont!
+
 echo Quantidade de videos faltando: !fcont! >> "%relatorio%"
+echo Minutos resto faltando: !resto!:!segfaltando! >> "%relatorio%"
 echo Quantidade de videos corrompidos: !xcont! >> "%relatorio%"
 echo Quantidade de videos desincronizados: !sinc! >> "%relatorio%"
+
+set /a cont=!cont!-1
+set /a horariosfaltando=(!sinc!*99)/10
+set /a totalsincronizados=(!cont!*99)/10
+set /a soma=!totalsincronizados!+!horariosfaltando!-!horafconf!+!horaiconf!-!minfaltando!
+
+set /a "prdanterior=!periodomin!-8"
+set /a "diferenca=!periodomin!-!soma!"
+
+set /a diferenca=!diferenca!/10
+
+if !fcont! gtr 0 if !tipovideo! equ 1 (
+  set /a "diferenca=!diferenca!-!fcont!"
+)
+
+if !diferenca! gtr 0 (
+ if !tipovideo! equ 1 (
+	echo Quantidade de videos desincronizados faltando: !diferenca! >> "%relatorio%"
+	echo !horainic! >> "%relatorio%"
+	echo Ultima data e horario de sincronização !ano!-!mes!-!dd!, !hh!:!mm!:!ss! >> "%relatorio%"
+ )
+) else (
+ if !tipovideo! equ 1 (
+	echo !horainic! >> "%relatorio%"
+	echo Ultima data e horario dos videos !ano!-!mes!-!dd!, !hh!:!mm!:!ss! >> "%relatorio%"
+ )
+)
+
+if !tipovideo! equ 2 (
+	echo !horainic! >> "%relatorio%"
+	echo Ultima data e horario dos videos !ano!-!mes!-!dd!, !hh!:!mm!:!ss! >> "%relatorio%"
+)
 
